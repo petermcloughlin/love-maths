41c5335d66449e8590f28872c83d1472e5c28a59
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-05-13 17:22:46 +0000

    Add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,9 @@ function runGame(gameType){
         displayMultiplyQuestion(num1, num2);
     }else if(gameType === "subtract"){
         displaySubractQuestion(num1, num2);
-    }else{
+    }else if(gameType === "division"){
+	    displayDivisionQuestion(num1, num2);
+	}else{
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -83,7 +85,9 @@ function calculateCorrectAnswer(){
         return [operand1 * operand2, "multiply"];
     }else if(operator === "-"){
         return [operand1 - operand2, "subtract"];
-    }else{
+    }else if(operator === "/") {
+	    return [Math.floor(operand1 / operand2), "division"];
+	}else{
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -124,4 +128,12 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "/";
+
 }
